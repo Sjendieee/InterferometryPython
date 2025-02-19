df276b493192f5e678491f3f6ff040b92ee8dac6
Reuvekamp <s.w.reuvekamp@utwente.nl>
2025-02-19 17:59:02 +0100

    for home

--- Fitting_spatial_CA_profiles_CoxVoinov.py
+++ Fitting_spatial_CA_profiles_CoxVoinov.py
@@ -432,7 +432,7 @@ def tiltedDropQuantitative():
     :return:
     """
     nr_of_datapoints = 1000
-    theta_eq_deg = np.ones(nr_of_datapoints) * 1.0            #deg
+    theta_eq_deg = np.ones(nr_of_datapoints) * 2.30            #deg
     v0 = 700 * 1E-6 / 60  #[m/s] assume same velocity at advancing and receding, just in opposite direction
 
     mu = 1.34 / 1000  # Pa*s
@@ -447,13 +447,13 @@ def tiltedDropQuantitative():
     #assuming v(0) = -v(pi)
     #velocity_local = np.cos(phi) * v0        #v0 at advancing, -v0 at receding
     #correcting for v(0) < -v(pi)
-    v_adv = 100 * 1E-6 / 60  #[m/s] assume same velocity at advancing and receding, just in opposite direction       [70]
-    v_rec = 40 * 1E-6 / 60  #[m/s] assume same velocity at advancing and receding, just in opposite direction      [150]
+    v_adv = 250 * 1E-6 / 60  #[m/s] assume same velocity at advancing and receding, just in opposite direction       [70]
+    v_rec = 135 * 1E-6 / 60  #[m/s] assume same velocity at advancing and receding, just in opposite direction      [150]
 
     velocity_local = np.array([np.cos(phi_l) * v_adv if abs(phi_l) < np.pi/2 else np.cos(phi_l) * v_rec for phi_l in phi])
-    fig2, ax2 = plt.subplots(1,2, figsize= (15, 9.6))
-    ax2[0].plot(phi, velocity_local)
-    ax2[0].set(xlabel='radial angle [rad]', ylabel='local velocity [m/s]', title='local velocity profile adjusted for difference in advancing and receding speed')
+    fig2, ax2 = plt.subplots(1,2, figsize= (15, 9.6/1.5))
+    ax2[0].plot(phi, velocity_local * 60 / 1E-6, linewidth=7)
+    ax2[0].set(xlabel='radial angle [rad]', ylabel=f'local velocity [$\mu$m/min]', title='Input local velocity profile \n(adjusted for difference between adv. and rec. speed)')
 
     Ca_local = mu * velocity_local / gamma
     print(f"Ca = {max(Ca_local)}")
@@ -463,19 +463,23 @@ def tiltedDropQuantitative():
     theta_app_calculated = np.power(np.power(theta_eq_rad, 3) + 9 * Ca_local * np.log(R/l), 1/3)
     theta_app_calculated_deg = theta_app_calculated * 180 / np.pi
 
-    ax2[1].plot(phi, theta_app_calculated_deg)
-    ax2[1].set(xlabel='radial angle [rad]', ylabel='CA_app [deg]', title='Calculated apparent contact angle profile')
+    ax2[1].plot(phi, theta_app_calculated_deg, color='darkorange', linewidth=7)
+    ax2[1].set(xlabel='radial angle [rad]', ylabel='CA$_{app}$ [deg]', title='Calculated apparent contact angle profile')
     fig2.tight_layout()
 
     R_drop = 1  # [mm]
     x_coord = R_drop * np.cos(phi)
     y_coord = R_drop * np.sin(phi)
-    fig1, ax1 = plt.subplots(figsize= (15, 9.6))
-    im1 = ax1.scatter(x_coord, y_coord, c=theta_app_calculated_deg, cmap='jet', vmin=min(theta_app_calculated_deg), vmax=max(theta_app_calculated_deg))
+    fig1, ax1 = plt.subplots(figsize= (12, 9.6))
+    im1 = ax1.scatter(x_coord, y_coord, s = 45, c=theta_app_calculated_deg, cmap='jet', vmin=min(theta_app_calculated_deg), vmax=max(theta_app_calculated_deg))
     ax1.set_xlabel("X-coord"); ax1.set_ylabel("Y-Coord");
     ax1.set_title(f"Spatial Contact Angles Colormap Tilted Droplet\n Quantitative description", fontsize=20)
     fig1.colorbar(im1)
     fig1.tight_layout()
+
+    fig2.savefig(os.path.join('C:\\Users\\ReuvekampSW\\Downloads', 'temp1.png'), dpi=600)
+    fig1.savefig(os.path.join('C:\\Users\\ReuvekampSW\\Downloads', 'temp2.png'), dpi=600)
+
     plt.show()
     return
 
